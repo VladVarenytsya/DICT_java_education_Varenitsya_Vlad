128be4259648682d5baecc8c2abace31afb508c2
VladVarenytsya <warenitsya33@gmail.com>
2022-10-28 18:46:03 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+public class ChatBot{
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Ducya.");
+        System.out.print("I was created in 2022.");
+    }
+
+}
+
+
