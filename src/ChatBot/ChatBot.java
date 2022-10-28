97154225c20f45774946407483f50a69722c8f3f
VladVarenytsya <warenitsya33@gmail.com>
2022-10-28 18:56:46 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,10 +1,16 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot{
 
     public static void main(String[] args) {
         System.out.println("Hello! My name is Ducya.");
-        System.out.print("I was created in 2022.");
+        System.out.println("I was created in 2022.");
+        System.out.println("Please, remind me your name.");
+        Scanner scan = new Scanner(System.in);
+        String userName = scan.nextLine();
+        System.out.println("What a great name you have," + userName + "!");
     }
 
 }
